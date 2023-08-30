4ff1affce6bcc71fed18c2cb7504dea90a9faa94
Patryk <pruswielki@gmail.com>
2023-08-30 15:43:02 +0200

    login styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 <script lang="ts">
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import '../app.css';
 
 	export let data;
 
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,8 +3,60 @@
 	export let form;
 </script>
 
-<form method="post" use:enhance>
-	<input name="email" value={form?.email ?? ''} />
-	<input type="password" name="password" />
-	<button>Sign up</button>
-</form>
+<main class="login-page-wrapper">
+	<form method="post" class="form-container" use:enhance>
+		<div class="input-label-container">
+			<h5 class="label">Email</h5>
+			<input name="email" value={form?.email ?? ''} />
+		</div>
+		<div class="input-label-container">
+			<h5 class="label">Password</h5>
+			<input type="password" name="password" />
+		</div>
+		<button>Login</button>
+	</form>
+</main>
+
+<style lang="scss">
+	.login-page-wrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100svh;
+	}
+	.form-container {
+		display: flex;
+		flex-direction: column;
+		gap: var(--size-fluid-3);
+		padding: 0 var(--size-fluid-1);
+	}
+
+	.input-label-container {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+	.label {
+		justify-self: center;
+		font-size: var(--font-size-fluid-1);
+		color: var(--text-1);
+		align-self: center;
+	}
+	input {
+		background-color: var(--surface-4);
+		padding: var(--size-fluid-1);
+		color: var(--text-1);
+	}
+	button {
+		background-color: var(--surface-3);
+		border-radius: var(--radius-1);
+		padding: var(--size-fluid-1);
+		transition: background-color 0.5s var(--ease-3);
+		font-size: var(--font-size-fluid-1);
+		color: var(--text-1);
+		font-weight: var(--font-weight-7);
+
+		&:hover {
+			background-color: var(--surface-4);
+		}
+	}
+</style>
