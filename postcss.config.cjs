36086ad8a9048d07777ac920b2cb0c16aceeb4fe
Patryk <pruswielki@gmail.com>
2023-09-01 17:58:27 +0200

    ship less props

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,8 +3,12 @@ const postcssPresetEnv = require('postcss-preset-env');
 const csso = require('postcss-csso');
 const postcssGlobalData = require('@csstools/postcss-global-data');
 const path = require('path');
+const postcssJitProps = require('postcss-jit-props');
 const config = {
 	plugins: [
+		postcssJitProps({
+			files: [path.resolve(__dirname, 'node_modules/open-props/open-props.min.css')]
+		}),
 		postcssGlobalData({
 			files: [path.resolve(__dirname, 'node_modules/open-props/media.min.css')]
 		}),
