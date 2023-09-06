c9cfd3a24fb03de43d14422107f464b024a5f6bc
Patryk <pruswielki@gmail.com>
2023-09-07 00:48:08 +0200

    nav

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -1,13 +1,38 @@
-<nav><h5>GN</h5></nav>
+<nav>
+	<h5>GN</h5>
+	<div class="links">
+		<a href="/train">Train</a><a href="/prepare">Prepare</a><a href="/analyze">Analyze</a>
+	</div>
+</nav>
 <slot />
 
 <style lang="postcss">
 	nav {
+		background-color: var(--surface-1);
+		backdrop-filter: blur(var(--radius-6));
+		padding: var(--size-3) var(--size-4);
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		display: flex;
+		gap: var(--size-fluid-6);
+		align-items: center;
+
+		@media (--sm-n-above) {
+		}
+		a {
+			color: var(--text-1);
+			font-weight: var(--font-weight-6);
+			font-size: var(--font-size-fluid-1);
+			&:hover {
+				color: var(--accent);
+			}
+		}
 	}
 
-	@media (--sm-n-above) {
-		nav {
-			background-color: var(--blue-10);
-		}
+	.links {
+		display: flex;
+		gap: var(--size-fluid-4);
 	}
 </style>
