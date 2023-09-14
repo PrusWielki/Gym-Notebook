87df207a2be62c7a5273d4a432165e9181c2dec8
Patryk <pruswielki@gmail.com>
2023-09-14 13:20:46 +0200

    soon

--- src/routes/(protected)/analyze/+page.svelte
+++ src/routes/(protected)/analyze/+page.svelte
@@ -1,9 +1,16 @@
-<h4>Coming Soonish</h4>
+<div class="wrapper">
+	<div class="container"><h3>Coming Soon</h3></div>
+</div>
 
 <style lang="postcss">
-	h4 {
+	.container {
+		display: flex;
+		justify-content: center;
+	}
+	h3 {
 		margin-top: var(--size-fluid-6);
 		text-align: center;
+		justify-self: center;
 		background: var(--gradient-3);
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
