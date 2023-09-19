fd459dd0b28a609cf5eec57ab028ac5535c4ee17
Patryk <pruswielki@gmail.com>
2023-09-19 16:47:51 +0200

    better login, register pages

--- src/routes/(with_homepage_navbar)/register/+page.svelte
+++ src/routes/(with_homepage_navbar)/register/+page.svelte
@@ -5,13 +5,9 @@
 
 <main class="login-page-wrapper">
 	<form method="post" class="form-container" use:enhance>
-		<div class="input-label-container">
-			<h5 class="label">Email</h5>
-			<input required name="email" value={form?.email ?? ''} />
-		</div>
-		<div class="input-label-container">
-			<h5 class="label">Password</h5>
-			<input required type="password" name="password" />
+		<div class="inputs-container">
+			<input required name="email" placeholder="Email" value={form?.email ?? ''} />
+			<input required type="password" placeholder="Password" name="password" />
 		</div>
 		<button>Register</button>
 	</form>
@@ -24,35 +20,34 @@
 		align-items: center;
 		height: 100svh;
 	}
+	.inputs-container {
+		display: flex;
+		flex-direction: column;
+		gap: var(--size-fluid-2);
+	}
 	.form-container {
 		display: flex;
 		flex-direction: column;
-		gap: var(--size-fluid-3);
-		padding: 0 var(--size-fluid-1);
+		gap: var(--size-fluid-4);
+		padding: var(--size-fluid-1);
 	}
 
-	.input-label-container {
-		display: grid;
-		gap: var(--size-fluid-1);
-		grid-template-columns: 1fr 1fr;
-	}
-	.label {
-		justify-self: center;
-		font-size: var(--font-size-fluid-2);
-		color: var(--text-1);
-		align-self: center;
-	}
 	input {
-		background-color: var(--surface-4);
+		background-color: var(--surface-3);
 		padding: var(--size-fluid-1);
 		color: var(--text-1);
+		font-size: var(--font-size-fluid-1);
+		&::placeholder {
+			color: var(--text-1);
+			text-align: center;
+		}
 	}
 	button {
 		background-color: var(--button-1);
 		border-radius: var(--radius-1);
 		padding: var(--size-fluid-1);
 		transition: background-color 0.5s var(--ease-3);
-		font-size: var(--font-size-fluid-1);
+		font-size: var(--font-size-fluid-2);
 		color: var(--text-1);
 		font-weight: var(--font-weight-7);
 
