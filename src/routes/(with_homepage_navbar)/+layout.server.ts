55daa01800151b43982bc7d42116414633f12da9
Patryk <pruswielki@gmail.com>
2023-09-19 17:32:51 +0200

    update types

--- src/routes/(with_homepage_navbar)/+layout.server.ts
+++ src/routes/(with_homepage_navbar)/+layout.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import type { LayoutServerLoad } from './$types.js';
+import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
 	const session = await getSession();
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types.js';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals: { getSession } }) => {
 	const session = await getSession();
