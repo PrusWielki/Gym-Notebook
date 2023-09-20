2d8453415035af6936eb3329e795f76b06d01a1d
Patryk <pruswielki@gmail.com>
2023-09-20 14:26:22 +0200

    fixes

--- src/lib/components/exercise_modal/exercise_modal.svelte
+++ src/lib/components/exercise_modal/exercise_modal.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	export let dialogOpened: Array<boolean>;
 	export let index: number;
-	export let exercises: Array<any>;
-	export let exercise: any;
+	export let exercises: Array<{ name: string }>;
+	export let exercise: { exercise_type_name: string };
 
 	let queryPhrase = '';
 </script>
@@ -12,14 +12,18 @@
 		<input bind:value={queryPhrase} placeholder="Search" />
 		<div class="types-container">
 			{#if exercises}
-				{#each exercises.filter( (exercise_type) => exercise_type.name.includes(queryPhrase) ) as exercise_types}
+				{#each exercises.filter((exercise_type) => exercise_type.name
+						.toLowerCase()
+						.includes(queryPhrase)) as exercise_types}
 					<!-- svelte-ignore a11y-click-events-have-key-events -->
 					<!-- svelte-ignore a11y-no-static-element-interactions -->
 					<div
 						on:click={() => {
 							exercise.exercise_type_name = exercise_types.name;
 							console.log(exercise.exercise_type_name);
+
 							dialogOpened[index] = false;
+							queryPhrase = '';
 						}}
 					>
 						{exercise_types.name}
@@ -51,5 +55,8 @@
 		display: flex;
 		flex-direction: column;
 		gap: var(--size-4);
+	}
+	input {
+		text-align: center;
 	}
 </style>
