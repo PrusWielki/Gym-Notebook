e4b554579606abbedc0412e8875964572ff32c91
Patryk <pruswielki@gmail.com>
2023-09-23 19:20:24 +0200

    progress

---
+++ src/lib/components/modals/browse_day_modal/browse_day_modal.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let planName: string;
+	export let modal: HTMLDialogElement;
+</script>
+
+<dialog bind:this={modal}>
+	<div>{planName}</div>
+</dialog>
+
+<style lang="postcss">
+	h3 {
+	}
+</style>
--- src/routes/(protected)/analyze/+page.svelte
+++ src/routes/(protected)/analyze/+page.svelte
@@ -1,11 +1,69 @@
+<script lang="ts">
+	import BrowseDayModal from '$lib/components/modals/browse_day_modal/browse_day_modal.svelte';
+	import type { PageData } from './$types';
+
+	let modal: HTMLDialogElement;
+	export let data: PageData;
+	let checked: 'Browse' | 'Statistics' = 'Browse';
+</script>
+
 <div class="wrapper">
-	<div class="container"><h3>Coming Soon</h3></div>
+	<div class="container">
+		<div class="analyze-container">
+			<div class="tabs">
+				<label class="tab">
+					<input
+						type="radio"
+						name="tab-input"
+						bind:group={checked}
+						value="Browse"
+						class="tab-input"
+					/>
+					<div class="tab-box">Browse</div>
+				</label>
+				<label class="tab">
+					<input
+						type="radio"
+						name="tab-input"
+						class="tab-input"
+						bind:group={checked}
+						value="Statistics"
+					/>
+					<div class="tab-box">Statistics</div>
+				</label>
+			</div>
+
+			{#if checked === 'Browse'}
+				{#await data.allData}
+					Loading...
+				{:then value}
+					{#if value.data}
+						<div
+							on:click={() => {
+								modal.showModal();
+							}}
+						>
+							{value.data[0].name}
+						</div>
+						<BrowseDayModal bind:modal planName={value.data[0].name} />
+					{/if}
+				{:catch error}
+					{error.message}
+				{/await}
+			{:else if checked === 'Statistics'}
+				Here
+			{/if}
+		</div>
+	</div>
 </div>
 
 <style lang="postcss">
-	.container {
+	.analyze-container {
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
+		align-items: center;
+		padding: var(--size-fluid-6) 0;
 	}
 	h3 {
 		margin-top: var(--size-fluid-6);
@@ -14,5 +72,41 @@
 		background: var(--gradient-3);
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
+	}
+
+	.tabs {
+		--bar-color: var(--accent);
+		--background: transparent;
+
+		font-size: var(--font-size-fluid-0);
+		display: flex;
+		width: 100%;
+		border: var(--border-size-1) solid var(--accent);
+		color: var(--text-1);
+	}
+
+	.tabs > .tab {
+		flex: 1;
+		display: flex;
+	}
+
+	.tab > .tab-input {
+		width: 0;
+		height: 0;
+		margin: 0;
+		display: none;
+	}
+
+	.tab > .tab-box {
+		padding: var(--size-3);
+		width: 100%;
+		text-align: center;
+		transition: 0.5s;
+		border-bottom: var(--border-size-3) solid rgba(0, 0, 0, 0);
+	}
+
+	.tab > .tab-input:checked + .tab-box {
+		background: var(--background);
+		border-color: var(--bar-color);
 	}
 </style>
