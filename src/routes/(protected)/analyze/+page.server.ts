c2717821eb9ea2af0fdd1a18c673d95d37c09405
Patryk <pruswielki@gmail.com>
2023-12-16 18:44:44 +0100

    upgrade to vite 5, sveltekit 2

--- src/routes/(protected)/analyze/+page.server.ts
+++ src/routes/(protected)/analyze/+page.server.ts
@@ -2,11 +2,11 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals: { supabase } }) => {
 	return {
-		allData: supabase
+		allData: await supabase
 			.from('Plans')
 			.select(
 				`id, name, periodization, custom, Weeks (id, order, Days (id, order, name, notes, Exercise_Detail (id, order, sets, target_reps, target_rpe, exercise_type_name, Exercise_Detail_Sets (id, set, reps, rpe, target_reps, target_rpe, weight, creation_date))))`
 			),
-		exerciseTypes: supabase.from('Exercise_Types').select()
+		exerciseTypes: await supabase.from('Exercise_Types').select()
 	};
 };
