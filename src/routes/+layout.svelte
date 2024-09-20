17cfba161b724209a8444cfdde06bf6de8a1aa77
PrusWielki <prusakpat@gmail.com>
2024-09-20 21:39:46 +0200

    firebase setup progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,43 @@
 <script lang="ts">
 	import '../app.css';
-	let { children } = $props();
+	import { onMount } from 'svelte';
+	import { session } from '$lib/session';
+	import { goto } from '$app/navigation';
+
+	let { children, data } = $props();
+
+	let loading: boolean = true;
+	let loggedIn: boolean = false;
+	let uid: string | null = null;
+
+	session.subscribe((cur: any) => {
+		loading = cur?.loading;
+		loggedIn = cur?.loggedIn;
+		uid = cur?.user.uid;
+	});
+
+	onMount(async () => {
+		try {
+			const user: any = await data.getAuthUser();
+
+			const loggedIn = !!user;
+			session.update((cur: any) => {
+				loading = false;
+				return {
+					...cur,
+					user,
+					loggedIn,
+					loading: false
+				};
+			});
+
+			if (loggedIn && uid) {
+				goto('/main');
+			}
+		} catch (ex) {
+			goto('/');
+		}
+	});
 </script>
 
 {@render children()}
