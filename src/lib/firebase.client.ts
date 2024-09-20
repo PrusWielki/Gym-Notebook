7a30290b65adc7de2b72aff92a4530952be842de
PrusWielki <prusakpat@gmail.com>
2024-09-20 21:35:01 +0200

    firebase setup progress

---
+++ src/lib/firebase.client.ts
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import type { FirebaseApp } from 'firebase/app';
+import type { Firestore } from 'firebase/firestore';
+import type { Auth } from 'firebase/auth';
+import { browser } from '$app/environment';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+export let db: Firestore;
+export let app: FirebaseApp;
+export let auth: Auth;
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
+    measurementId: import.meta.env.VITE_MEASURMENTID
+};
+
+// Initialize Firebase
+export const initializeFirebase = () => {
+    if (!browser) {
+        throw new Error("Can't use the Firebase client on the server.");
+    }
+    if (!app) {
+        app = initializeApp(firebaseConfig);
+        auth = getAuth(app);
+
+    }
+};
