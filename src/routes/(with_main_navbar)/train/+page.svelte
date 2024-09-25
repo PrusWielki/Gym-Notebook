2b5113d4ebb8c1b711dbf8af9e90c5987d030725
PrusWielki <prusakpat@gmail.com>
2024-09-25 18:01:51 +0200

    main page progress

--- src/routes/(with_main_navbar)/train/+page.svelte
+++ src/routes/(with_main_navbar)/train/+page.svelte
@@ -1,80 +1,28 @@
 <script lang="ts">
-	import Card from '$lib/components/card/card.svelte';
-	import CardModal from '$lib/components/card_modal/card_modal.svelte';
-	import { collection, getFirestore, onSnapshot, orderBy, query, where } from 'firebase/firestore';
-	import type { QueryDocumentSnapshot } from 'firebase/firestore/lite';
-	import { session } from '$lib/session';
-	import { app } from '$lib/firebase.client';
-	import { onDestroy } from 'svelte';
-
-	let uid: string | null | undefined = null;
-
-	session.subscribe((value) => (uid = value?.user?.uid));
-	let db = null;
-	let collectionRef = null;
-
-	if (app) {
-		db = getFirestore();
-		collectionRef = collection(db, 'userData');
-	}
-
-	let queryResult: QueryDocumentSnapshot[] | null = null;
-
-	let openModal = false;
-	let currentText = '';
-	let currentImage = '';
-	let currentSummary = '';
-	let currentId: string | null = null;
-	let unsubscribe: (() => void) | null = null;
-
-	const getData = (uid: string | null | undefined) => {
-		if (uid && uid !== '' && collectionRef) {
-			const q = query(collectionRef, where('userId', '==', uid), orderBy('date'));
-			unsubscribe = onSnapshot(q, (snapshot) => {
-				queryResult = snapshot.docs.reverse();
-			});
-		}
-	};
-
-	$: {
-		getData(uid);
-	}
-
-	onDestroy(() => {
-		if (unsubscribe) unsubscribe();
-	});
+	let selectedWeek = $state(0);
+	let selectedDay = $state(0);
 </script>
 
-<div class="h-[100dvh] w-full">
-	<div class="mx-auto max-w-screen-xl px-4 py-20">
-		<div class="flex flex-row flex-wrap justify-center gap-2">
-			<!-- <button class="btn-primary btn">Add a new note</button> -->
-			{#if queryResult}
-				{#each queryResult as doc}
-					{@const { text, summary, imageName } = doc.data()}
-
-					<button
-						on:click={() => {
-							openModal = false;
-							openModal = true;
-							currentText = text;
-							currentSummary = summary;
-							currentImage = imageName;
-							currentId = doc.id;
-						}}
-						class="w-full sm:w-96"
-					>
-						<Card {text} />
-					</button>
-				{/each}
-			{/if}
-		</div>
-		<CardModal
-			bind:open={openModal}
-			summary={currentSummary}
-			image={currentImage}
-			text={currentText}
-			docId={currentId}
-		/>
+<section class="h-[100dvh] w-full">
+	<div class="mx-auto max-w-screen-xl px-4 py-16 lg:py-20">
+		<section class="flex flex-col items-center gap-2 lg:flex-row lg:justify-center">
+			<h1 class="text-base font-semibold lg:text-xl">Plan Name</h1>
+			<div class="flex flex-row gap-2 lg:inline-flex">
+				<select
+					class="select select-primary"
+					bind:value={selectedWeek}
+					onchange={(e) => {
+						selectedWeek = +e.currentTarget.value;
+					}}><option disabled value={0}>Week 1</option></select
+				>
+				<select
+					class="select select-primary"
+					bind:value={selectedDay}
+					onchange={(e) => {
+						selectedDay = +e.currentTarget.value;
+					}}><option disabled value={0}>Day 1</option></select
+				>
+			</div>
+		</section>
 	</div>
-</div>
+</section>
