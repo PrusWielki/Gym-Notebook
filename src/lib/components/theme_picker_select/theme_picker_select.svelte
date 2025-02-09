075ba72710b8165f832619f7ee6a7284c9f95b75
PrusWielki <prusakpat@gmail.com>
2025-02-09 18:51:48 +0100

    update layout

--- src/lib/components/theme_picker_select/theme_picker_select.svelte
+++ src/lib/components/theme_picker_select/theme_picker_select.svelte
@@ -20,41 +20,16 @@
 	let selectedTheme = theme;
 </script>
 
-<div
-	class="relative w-full h-full btn btn-neutral btn-ghost btn-sm active:focus:scale-100 active:hover:scale-100"
+<select
+	class={`select select-neutral select-ghost select-sm  h-full w-full text-center ${className}`}
+	bind:value={selectedTheme}
+	on:change={() => {
+		if (selectedTheme) onThemeSelect(selectedTheme);
+	}}
 >
-	<select
-		class={`appearance-none absolute btn w-full h-full  btn-neutral btn-ghost btn-sm active:focus:scale-100  active:hover:scale-100 ${className}`}
-		bind:value={selectedTheme}
-		on:change={() => {
-			if (selectedTheme) onThemeSelect(selectedTheme);
-		}}
-	>
-		<option class="bg-base-100" disabled value="theme">Theme</option>
+	<option class="bg-base-100" disabled value="theme">Theme</option>
 
-		{#each themes as themeName}
-			<option class="bg-base-100" value={themeName}>{themeName}</option>
-		{/each}
-	</select>
-	<div class="absolute right-2 fill-neutral-content">
-		<svg
-			height="8px"
-			width="8px"
-			version="1.1"
-			id="Layer_1"
-			xmlns="http://www.w3.org/2000/svg"
-			xmlns:xlink="http://www.w3.org/1999/xlink"
-			viewBox="0 0 407.437 407.437"
-			xml:space="preserve"
-			><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
-				id="SVGRepo_tracerCarrier"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			></g><g id="SVGRepo_iconCarrier">
-				<polygon
-					points="386.258,91.567 203.718,273.512 21.179,91.567 0,112.815 203.718,315.87 407.437,112.815 "
-				></polygon>
-			</g></svg
-		>
-	</div>
-</div>
+	{#each themes as themeName}
+		<option class="bg-base-100" value={themeName}>{themeName}</option>
+	{/each}
+</select>
--- src/routes/(with_main_navbar)/+layout.svelte
+++ src/routes/(with_main_navbar)/+layout.svelte
@@ -15,10 +15,10 @@
 			else if (value && value.loggedIn === true) loggedIn = true;
 		});
 	let resultsMenu: HTMLDetailsElement;
-	if (browser)
-		document.addEventListener('click', function () {
-			if (resultsMenu && resultsMenu.removeAttribute) resultsMenu.removeAttribute('open');
-		});
+	// if (browser)
+	// 	document.addEventListener('click', function () {
+	// 		if (resultsMenu && resultsMenu.removeAttribute) resultsMenu.removeAttribute('open');
+	// 	});
 </script>
 
 {#if loggedIn}
@@ -26,60 +26,35 @@
 		<div class="navbar mx-auto min-h-0 max-w-(--breakpoint-xl) px-4">
 			<div class="navbar-start w-fit lg:w-1/2">
 				<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-				<details
-					class="dropdown"
-					on:click={(e) => {
-						if (resultsMenu.open) {
-							e.stopPropagation();
-						}
-					}}
-					on:keydown={(e) => {
-						if (resultsMenu.open) {
-							e.stopPropagation();
-						}
-					}}
-					bind:this={resultsMenu}
+				<button class="btn" popovertarget="popover-1" style="anchor-name:--anchor-1">
+					Button
+				</button>
+				<ul
+					class="dropdown menu rounded-box bg-base-100 w-52 shadow-sm"
+					popover="auto"
+					id="popover-1"
+					style="position-anchor:--anchor-1"
 				>
-					<summary tabindex="0" class="btn btn-ghost btn-sm lg:btn-md lg:hidden">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-5 w-5"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							><path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M4 6h16M4 12h8m-8 6h16"
-							/></svg
+					<li>
+						<ThemePickerSelect class="my-2" />
+					</li>
+					<li>
+						<button
+							on:click={() => {
+								const auth = getAuth();
+								signOut(auth)
+									.then(() => {
+										goto('/');
+									})
+									.catch(() => {
+										showNotification('Logout Error!', 2000, 'Failure');
+									});
+							}}
+							class="btn btn-ghost btn-secondary btn-sm">Logout</button
 						>
-					</summary>
-					<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
-					<ul
-						tabindex="0"
-						class="menu dropdown-content menu-sm rounded-box bg-base-200 z-1 mt-3 w-52 gap-4 p-2 shadow-sm"
-					>
-						<li>
-							<ThemePickerSelect class="" />
-						</li>
-						<li>
-							<button
-								on:click={() => {
-									const auth = getAuth();
-									signOut(auth)
-										.then(() => {
-											goto('/');
-										})
-										.catch(() => {
-											showNotification('Logout Error!', 2000, 'Failure');
-										});
-								}}
-								class="btn btn-ghost btn-secondary btn-sm">Logout</button
-							>
-						</li>
-					</ul>
-				</details>
+					</li>
+				</ul>
+
 				<a href="/train" class="btn btn-ghost hidden text-xl font-semibold lg:flex">Gym Notebook</a>
 			</div>
 			<div class="navbar-center mx-auto gap-2 lg:mx-0">
